4afcda8c2d7d972839eba5814cf493a0685ebcd8
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-12-03 19:58:10 -0300

    changes on demian name

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const pathoSpotterApi = axios.create({
-  baseURL: "http://localhost:8000/v1",
+  // baseURL: "http://localhost:8000/v1",
+  baseURL: "http://terumo-api:8000/v1",
 });
